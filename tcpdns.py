cde5b72ef3d76d9068c58664393de875ae00e62c
hfsz2313 <hfsz2313_1@outlook.com>
2023-11-22 11:58:47 +0800

    modified:   tcpdns.py

--- tcpdns.py
+++ tcpdns.py
@@ -3,9 +3,7 @@ l=open('tcpdns.in','r').read().split()
 str=""
 for dns in l:
     p=Ping(dns,53,3)
-    #q=Ping(dns,80,3)
     p.ping(1)
-    #q.ping(1)
     if p.__dict__['_successed']:
         str+='"'+dns+':53",\n'
 open('tcpdns.out','w').write(str)
