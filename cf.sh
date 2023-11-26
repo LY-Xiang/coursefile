29bc7f4a4de21cc771bc35e4568dd7d3ac3b8872
LY-Xiang <3047563007@qq.com>
2023-11-26 13:42:21 +0800

    autocommit

---
+++ cf.sh
@@ -0,0 +1,10 @@
+err=0
+echo "~~>git pull..." && git pull
+err=$err+$?
+echo "~~>git add..." && git add .
+err=$err+$?
+echo "~~>git commit..." && git commit -m "autocommit"
+err=$err+$?
+echo "~~>git push..." && git push
+err=$err+$?
+echo "-->exit..." && exit $err
