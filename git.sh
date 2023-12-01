ca467006d6d60b8cadb71633c2188dc46a5004ea
LY-Xiang <3047563007@qq.com>
2023-12-01 23:21:29 +0800

    autocommit

--- git.sh
+++ git.sh
@@ -3,7 +3,6 @@ echo "-->set val'err'... " && typeset -i err=0
 echo "~~>git pull..." && git pull
 err=$err+$?
 echo "~~>git add..." && git add .
-err=$err+$?
 echo "~~>git commit..." && git commit -m "autocommit"
 err=$err+$?
 echo "~~>git push..." && git push
