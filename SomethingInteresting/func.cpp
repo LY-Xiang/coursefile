385c79004588540c237bb4c8ff2614a2212b67e2
LY-Xiang <3047563007@qq.com>
2023-12-02 20:52:11 +0800

    Auto Commit:Sat Dec  2 20:52:11 CST 2023

---
+++ SomethingInteresting/func.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+class bop{
+    int n[4];
+    bop(int a, int b)
+    {
+	n[0]=a+b;
+	n[1]=a-b;
+	n[2]=a*b;
+	n[3]=a/b;
+    }
+    bop operator ()(void)
+    {
+	return *this;
+    }
+};
+int main()
+{
+    int a[4]={0};
+    a=bop(7, 2)();
+    printf("%d%d%d%d", a[0], a[1], a[2], a[3]);
+    return 0;
+}
