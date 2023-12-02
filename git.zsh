1d62211f279e5e001f2a7996fe393993a8c16b6f
LY-Xiang <3047563007@qq.com>
2023-12-02 17:06:24 +0800

    Auto Commit:Sat Dec  2 17:06:24 CST 2023

--- git.zsh
+++ git.zsh
@@ -1,10 +1,7 @@
 #!/bin/zsh
-echo "-->set val'err'... " && typeset -i err=0
-echo "~~>git pull..." && git pull
-err=$err+$?
-echo "~~>git add..." && git add .
-echo "~~>git commit..." && git commit -m "Auto Commit:$(date)"
-err=$err+$?
-echo "~~>git push..." && git push
-err=$err+$?
+echo "-->start git..." && typeset -i err=0
+echo "~~>git pull..." && git pull && err=$err+$?
+echo "~~>git add..." && git add . && err=$err+$?
+echo "~~>git commit..." && git commit -m "Auto Commit:$(date)" && err=$err+$?
+echo "~~>git push..." && git push && err=$err+$?
 echo "-->exit..." && exit $err
