7983a0bc9f2372fc48a14b67d6db6fef2b5aca54
LY-Xiang <3047563007@qq.com>
2023-12-02 13:10:51 +0800

    autocommit

--- 进制转换.cpp
+++ 进制转换.cpp
@@ -1,13 +1,19 @@
 #include <cstdio>
 int main()
 {
-	unsigned short s[64], w, l;
-	unsigned long long n, r;
-	for (scanf("%llu", &n); n; n--)
+	unsigned short s[64], *w, W, l;
+	unsigned long long n, i, *r, R;
+	scanf("%llu", &n);
+	w = new unsigned short[n];
+	r = new unsigned long long[n];
+	for (i = 0; i < n; i++)
+		scanf("%llu%hu", r + i, w + i);
+	for (i = 0; i < n; i++)
 	{
-		scanf("%llu%hu", &r, &w);
-		for (l = 0; r; r /= w)
-			s[l++] = r % w;
+		R = r[i];
+		W = w[i];
+		for (l = 0; R; R /= W)
+			s[l++] = R % W;
 		for (; l; l--)
 			putchar(s[l - 1] + (s[l - 1] < 10 ? '0' : 'A'));
 		putchar('\n');
