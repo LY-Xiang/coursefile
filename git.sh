e3077ae2c702d1a5af281319444ee4dd1bb7b438
LY-Xiang <3047563007@qq.com>
2023-12-02 16:04:45 +0800

    Auto Commit:Sat Dec  2 16:04:45 CST 2023

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@ echo "-->set val'err'... " && typeset -i err=0
 echo "~~>git pull..." && git pull
 err=$err+$?
 echo "~~>git add..." && git add .
-echo "~~>git commit..." && git commit -m "autocommit"
+echo "~~>git commit..." && git commit -m "Auto Commit:$(date)"
 err=$err+$?
 echo "~~>git push..." && git push
 err=$err+$?
