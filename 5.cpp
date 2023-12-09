a71c8651b1070195a9f6ac16ede2522ccdbe6d64
LY-Xiang <3047563007@qq.com>
2023-12-09 21:12:56 +0800

    Auto Commit:Sat Dec  9 21:12:56 CST 2023

--- 5.cpp
+++ 5.cpp
@@ -1,33 +1,38 @@
 #include <cstdio>
-typedef unsigned long long LLU;
-void exgcd(LLU,LLU,LLU&,LLU&);
-int main(void)
+typedef signed long long LLD;
+template <class T>
+void exgcd(T, T, T &, T &);
+int main()
 {
-	LLU n,a=0,i,t,x,y;
-	const unsigned long long m=998244353;
-	scanf("%llu",&n);
-	for(i=0;i<n;i++)
+	LLD n, a = 0, i, x, y;
+	const LLD m = 998244353;
+	scanf("%lld", &n);
+	for (i = 0; i < n; i++)
 	{
-		scanf("%llu",&t);
-		a=a+t%m;
+		scanf("%lld", &y);
+		a = (a + y) % m;
 	}
-	for(i=1;i<=n;i++)
+	exgcd<LLD>(n, m, x, y);
+	x = (x % m + m) % m * a % m;
+	for (i = 1; i <= n; i++)
 	{
-		t=i*n%m;
-		exgcd(t,a,x,y);
-		printf("%llu ",x);
+		printf("%lld ", i * x % m);
 	}
 	return 0;
 }
-void exgcd(LLU a,LLU b,LLU &x,LLU &y)
+template <class T>
+void exgcd(T a, T b, T &x, T &y)
 {
-    if(!b)
-    {
-        x=1;y=0;
-    }
-    else
-    {
-        exgcd(b,a%b,y,x);
-        y-=a/b*x;
-    }
+	if (!b)
+	{
+		x = 1;
+		y = 0;
+	}
+	else
+	{
+		exgcd(b, a % b, y, x);
+		y -= a / b * x;
+	}
+	return;
+}n;
 }
