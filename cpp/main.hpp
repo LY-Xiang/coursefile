9f5cc36d11ef30727cb6c18b1d1ffc8818c17bc1
hfsz2319 <hfsz2319_1@outlook.com>
2024-10-24 11:41:34 +0800

    1

--- cpp/main.hpp
+++ cpp/main.hpp
@@ -1,9 +1,12 @@
 #ifndef XLY_MAIN_HPP
 #define XLY_MAIN_HPP
-#include<map>
-#include<string>
+#include <map>
+#include <string>
 typedef int fmain();
-struct smain{
-static std::map<std::string,fmain*> menu;
-smain(const std::string &name,fmain* prog){menu[name]=prog;}};
-#endif 
+struct smain
+{
+    static std::map<std::string, fmain *> menu;
+    smain(const std::string &name, fmain *prog) { menu[name] = prog; }
+};
+std::map<std::string, fmain *> smain::menu = std::map<std::string, fmain *>();
+#endif
